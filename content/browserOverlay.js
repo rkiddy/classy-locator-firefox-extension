6c4303622802c912744bf878cb7a4596c774af61
Ray Kiddy <ray@ganymede.org>
2014-10-30 12:29:02 -0700

    Is now writing to a file in /tmp.

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -3,6 +3,13 @@
  */
 if ("undefined" == typeof(GanymedeOrg)) {
   var GanymedeOrg = {};
+
+  var file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("TmpD", Components.interfaces.nsIFile);
+
+  file.append("test.txt");
+
+  var outputStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
+  outputStream.init(file, 0x04 | 0x08 | 0x10, 0644, 0);
 };
 
 /**
@@ -10,13 +17,17 @@ if ("undefined" == typeof(GanymedeOrg)) {
  */
 GanymedeOrg.BrowserOverlay = {
 
-  /**
-   * Says 'Hello' to the user.
-   */
   findClassLabels : function(aEvent) {
 
+    var console = window.content.document.console;
+
     let stringBundle = document.getElementById("classylocator-string-bundle");
-    let message = stringBundle.getString("classylocator.greeting.label");
+    let message = stringBundle.getString("classylocator.greeting.label") + "(" + console + ")";
+
+    var text = "Text to be written\n";
+
+    outputStream.write(text, text.length);
+
     window.alert(message);
   }
 };
