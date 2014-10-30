1e8eb10409d97fad8a17938b1e289ad26bb67534
Ray Kiddy <ray@ganymede.org>
2014-10-30 13:34:03 -0700

    Catching the page load not working but should, is implemented.

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -1,33 +1,74 @@
-/**
- * GanymedeOrg namespace.
- */
+
 if ("undefined" == typeof(GanymedeOrg)) {
+
   var GanymedeOrg = {};
 
-  var file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("TmpD", Components.interfaces.nsIFile);
-
-  file.append("test.txt");
-
-  var outputStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
-  outputStream.init(file, 0x04 | 0x08 | 0x10, 0644, 0);
+  var file = null;
+  var oStream = null;
 };
 
-/**
- * Controls the browser overlay for the Hello World extension.
- */
 GanymedeOrg.BrowserOverlay = {
 
-  findClassLabels : function(aEvent) {
+  startRecording : function(anEvent) {
 
-    var console = window.content.document.console;
+    if (file == null) {
 
-    let stringBundle = document.getElementById("classylocator-string-bundle");
-    let message = stringBundle.getString("classylocator.greeting.label") + "(" + console + ")";
+      file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("TmpD", Components.interfaces.nsIFile);
+      file.append("test.txt");
 
-    var text = "Text to be written\n";
+      oStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
+      oStream.init(file, 0x04 | 0x08 | 0x10, 0644, 0);
 
-    outputStream.write(text, text.length);
+      var msg = "\nStart of recoding: " + Date() + " *******\n\n";
 
-    window.alert(message);
+      oStream.write(msg, msg.length);
+
+      //window.addEventListener('load', findClassLabels, false);
+
+      window.alert("starting...");
+
+    } else {
+      window.alert("already started.");
+    }
+  },
+
+  stopRecording : function(anEvent) {
+
+    var msg = "\nStopped recoding: " + Date() + " *******\n\n";
+
+    oStream.write(msg, msg.length);
+
+    oStream.close();
+
+    oStream = null;
+
+    file = null;
+
+    //window.removeEventListener('load', findClassLabels, false);
+
+    window.alert("stopping...");
+  },
+
+  findClassLabels : function(anEvent) {
+
+    oStream.write("ok\n", 3);
+
+/*
+    let doc = event.originalTarget;
+
+    // Make sure the doc is not in a frame, is top doc.
+    if (doc instanceof HTMLDocument) {
+      if (doc.defaultView.frameElement) {
+        while (doc.defaultView.frameElement) {
+          doc = doc.defaultView.frameElement.ownerDocument;
+        }
+      }
+    }
+
+    var text = "At " + Date() + ", Page = " + doc.title + "\n";
+
+    oStream.write(text, text.length);
+*/
+    window.alert("page!");
   }
 };
